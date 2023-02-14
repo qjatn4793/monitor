00e7c70793896f94f518c4cc2de770c1d6856220
qjatn4793 <qjatn4793@naver.com>
2023-02-14 11:12:12 +0900

    20230214 김범수 엑츄에이터 클라이언트단 구현 > 포트수정 및 오류개선

--- src/main/java/kr/securus/monitor/MonitorApplication.java
+++ src/main/java/kr/securus/monitor/MonitorApplication.java
@@ -1,11 +1,9 @@
 package kr.securus.monitor;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 public class MonitorApplication {
 
 	public static void main(String[] args) {
